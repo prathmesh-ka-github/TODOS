9b47c9325d2a290e93717451b89bee63db0aec7e
Prathmesh Kale <connect.prathmesh905@gmail.com>
2022-12-24 00:01:40 +0530

    added components and linked to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Nav from './Components/Nav'
+import Mucho from './Components/Mucho'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-
+      <Nav/>
+      <Mucho/>
     </div>
   )
 }
---
+++ src/Components/Mucho.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mucho = () => {
+  return (
+    <div>Mucho</div>
+  )
+}
+
+export default Mucho
