c4cf29e87ac667ae56a00ce4e4ed8df781835b01
Prathmesh Kale <connect.prathmesh905@gmail.com>
2023-02-08 22:13:03 +0530

    started with js functions

--- src/script.js
+++ src/script.js
@@ -1,1 +1,20 @@
-console.log("Hi mom!")
+const listContainer = document.getElementById('todo-containers')
+
+let list = ['Gym','Todo']
+
+function render(){
+    clearElement(listContainer)
+    console.log("render function working!")
+    list.forEach(list => {
+        var listElement = document.createElement('li')
+        listElement.classList.add()
+        listElement.innerText = list
+        listContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+    console.log("clear function working!")
+}
+console.log(listContainer)
+render()
