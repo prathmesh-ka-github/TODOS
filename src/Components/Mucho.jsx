d7eb50a39327ac4d490b91ee7d3d7709d61cc799
Prathmesh Kale <connect.prathmesh905@gmail.com>
2023-02-18 17:35:35 +0530

    useState added and new order in todo container

--- src/Components/Mucho.jsx
+++ src/Components/Mucho.jsx
@@ -1,84 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Mucho = () => {
+
+  const [inputList, setInputList] = useState("");
+  const item = () => {
+
+  };
   return (
     <div className='main-container'>
-        <div className='all-lists'>
-          <h2>My lists.</h2>
-          <ul id='todo-container' className='task-list'>
-            <li className='list-name active'>Projects</li>
-            <li className='list-name'>College</li>
-            <li className='list-name'>Grocery</li>
-          </ul>
+      <div className='all-lists'>
+        <h2>My lists.</h2>
+        <ul id='todo-container' className='task-list'>
+          <li className='list-name active'>Projects</li>
+          <li className='list-name'>College</li>
+          <li className='list-name'>Grocery</li>
+        </ul>
 
+        <form action="">
+          <button className="btn-new-list">+</button>
+          <input type="text" className='new-list' onChange={item} placeholder='New list' />
+        </form>
+      </div>
+
+      <div className='todo-list-container'>
+        <div className="todo-header">
+          <h2 className="list-title">Youtube</h2>
+          <p className="task-count">3 task remaining</p>
+        </div>
+        {/*-------------------- ENTER INPUT FIELD ----------------------*/}
+        <div className="new-task-create">
           <form action="">
             <button className="btn-new-list">+</button>
             <input type="text" className='new-list'
-            placeholder='New list'/>
+              placeholder='New task' />
           </form>
         </div>
 
-        <div className='todo-list-container'>
-          <div className="todo-header">
-            <h2 className="list-title">Youtube</h2>
-            <p className="task-count">3 task remaining</p>
-          </div>
-          <div className="todo-body">
-            {/* -------------------------lists here----------------------- */}
-            <div className="tasks">
-              <div className="task">
-                <input type="checkbox"/>
-                <label>
-                  <span className="custom-checkbox"></span>
-                  Make a todos app's UI
-                </label>
-              </div>
-            </div>
 
-            <div className="tasks">
-              <div className="task">
-                <input type="checkbox"/>
-                <label>
-                  <span className="custom-checkbox"></span>
-                  Review the changes
-                </label>
-              </div>
-            </div>
-            <div className="tasks">
-              <div className="task">
-                <input type="checkbox"/>
-                <label>
-                  <span className="custom-checkbox"></span>
-                  Push the changes
-                </label>
-              </div>
-            </div>
-
-            <div className="tasks">
-              <div className="task">
-                <input type="checkbox"/>
-                <label>
-                  <span className="custom-checkbox"></span>
-                  Create a pull request
-                </label>
-              </div>
+        {/* -------------------------lists here----------------------- */}
+        <div className="todo-body">
+          <div className="tasks">
+            <div className="task">
+              <input type="checkbox" />
+              <label>
+                <span className="custom-checkbox"></span>
+                Make a todos app's UI
+              </label>
             </div>
           </div>
-            {/*-------------------- ENTER INPUT FIELD ----------------------*/}
-            <div className="new-task-create">
-              <form action="">
-                <button className="btn-new-list">+</button>
-                <input type="text" className='new-list' 
-                placeholder='New task' />
-              </form>
-            </div>
 
-            <div className="delete-stuff">
-              <button className="btn delete">Clear completed tasks</button>
-              <button className="btn delete delete-btn">Delete list</button>
+          <div className="tasks">
+            <div className="task">
+              <input type="checkbox" />
+              <label>
+                <span className="custom-checkbox"></span>
+                Review the changes
+              </label>
             </div>
+          </div>
+          <div className="tasks">
+            <div className="task">
+              <input type="checkbox" />
+              <label>
+                <span className="custom-checkbox"></span>
+                Push the changes
+              </label>
+            </div>
+          </div>
 
+          <div className="tasks">
+            <div className="task">
+              <input type="checkbox" />
+              <label>
+                <span className="custom-checkbox"></span>
+                Create a pull request
+              </label>
+            </div>
+          </div>
         </div>
+        <div className="delete-stuff">
+          <button className="btn delete">Clear completed tasks</button>
+          <button className="btn delete delete-btn">Delete list</button>
+        </div>
+      </div>
     </div>
   )
 }
--- src/script.js
+++ src/script.js
@@ -1,21 +1,21 @@
-const listContainer = document.getElementById('todo-containers')
+// const listContainer = document.getElementById('todo-containers')
 
-let list = ['Gym','Todo']
+// let list = ['Gym','Todo']
 
-// to render the given list.
-function render(){
-    clearElement(listContainer)
-    console.log("render function working!")
-    list.forEach(list => {
-        var listElement = document.createElement('li')
-        listElement.classList.add()
-        listElement.innerText = list
-        listContainer.appendChild(listElement)
-    })
-}
+// // to render the given list.
+// function render(){
+//     clearElement(listContainer)
+//     console.log("render function working!")
+//     list.forEach(list => {
+//         var listElement = document.createElement('li')
+//         listElement.classList.add()
+//         listElement.innerText = list
+//         listContainer.appendChild(listElement)
+//     })
+// }
 
-function clearElement(element) {
-    console.log("clear function working!")
-}
-console.log(listContainer)
-render()
+// function clearElement(element) {
+//     console.log("clear function working!")
+// }
+// console.log(listContainer)
+// render()
